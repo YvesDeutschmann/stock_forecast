050a7cf27c53a29c6a70d49f40b9939d78bd4a31
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-08-26 16:24:32 +0200

    feat: Added load_data step

---
+++ data/get_data.py
@@ -0,0 +1,43 @@
+import finnhub
+import pandas as pd
+from datetime import datetime
+
+# Setup client (Insert your own key here)
+finnhub_client = finnhub.Client(api_key="bsrr9nn48v6u62sfl8cg")
+
+def convert_to_unix(timestamp):
+    """Converts a pandas timestamp to a unix integer."""
+    return (timestamp - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+
+def convert_to_timestamp(unix):
+    """Converts a unix integer into a pandas datetime object."""
+    return pd.to_datetime(unix, unit='s')
+
+def load_data(symbols, timedelta='5y'):
+    """
+    Queries list of stock symbols for their OHLC data.
+    Timeframe is from current day back until the desired timeframe (default: 5 years).
+    Start
+    Arguments:
+    symbols - list of strings containing the stock symbol of the desired stock
+    timedelta (optional) - timeframe to query data in the past
+    Returns:
+    ohlc_data - dict of dataframes containing ohlc data for each symbol over certain timeframe
+    """
+    ohlc_data = dict()
+    # get start & endtime for ohlc data
+    start_time = convert_to_unix(datetime.now() - pd.Timedelta(timedelta))
+    end_time = convert_to_unix(datetime.now())
+    # set resolution for query to 'Daily'
+    resolution = 'D'
+        
+    # get OHLC data for each symbol
+    for symbol in symbols:
+        res = finnhub_client.stock_candles(symbol, resolution, start_time, end_time)
+
+        data = pd.DataFrame(res)
+        data = data.set_index(convert_to_timestamp(data.t))
+
+        ohlc_data.update({symbol: data})
+    
+    return ohlc_data
